d4f96071ee8592607525b3575cd32dd8668585e7
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2026-03-31 02:53:26 -0300

    feat: add motorista and viagem tables with foreign key constraint

--- empresa-de-onibus/02_create_tables.sql
+++ empresa-de-onibus/02_create_tables.sql
@@ -1,6 +1,6 @@
 USE empresa_onibus;
 
--- Tabela: características do veículo
+-- Características do veículo
 CREATE TABLE onibus (
     id_onibus          INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     modelo             VARCHAR(20)     NOT NULL,
@@ -9,3 +9,21 @@ CREATE TABLE onibus (
     som                CHAR(3)         NOT NULL,
     ar_condicionado    CHAR(3)         NOT NULL
 );
+
+-- Informações sobre o motorista
+CREATE TABLE motorista(
+id_motorista 		   INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nome_motorista 		   VARCHAR(50) NOT NULL
+);
+
+-- Informações sobre viagem
+CREATE TABLE viagem(
+id_viagem 			  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+local_partida	      VARCHAR(20) NOT NULL,
+local_destino		  VARCHAR(20) NOT NULL,
+valor		  		  DECIMAL(10,2) NOT NULL,
+id_motorista 		  INT NOT NULL,
+
+CONSTRAINT fk_motorista FOREIGN KEY (id_motorista)
+REFERENCES motorista(id_motorista)
+); 
